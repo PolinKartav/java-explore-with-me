57e22210aada04e0b026514918cccb61fd1c827b
PolinKartav <polyakarpushina@yandex.ru>
2023-09-20 22:30:51 +0300

    fix: style

--- statistics/service/src/main/java/ru/practicum/statistics/service/StatisticsServiceImpl.java
+++ statistics/service/src/main/java/ru/practicum/statistics/service/StatisticsServiceImpl.java
@@ -16,7 +16,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class StatisticsServiceImpl implements StatisticsService{
+public class StatisticsServiceImpl implements StatisticsService {
     private final StatisticsRepository repository;
 
     @Override
