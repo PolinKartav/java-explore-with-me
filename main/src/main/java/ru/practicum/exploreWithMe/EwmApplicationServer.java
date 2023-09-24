4ff7b77d08268dff30ada6c68476f3b10ab1d6eb
PolinKartav <polyakarpushina@yandex.ru>
2023-09-24 23:52:01 +0300

    fixed pom, added modul

---
+++ main/src/main/java/ru/practicum/exploreWithMe/EwmApplicationServer.java
@@ -0,0 +1,9 @@
+package ru.practicum.exploreWithMe;
+
+
+public class EwmApplicationServer {
+    public static void main(String[] args) {
+        System.out.println("Пуккккккккк!!!!!");
+    }
+
+}
